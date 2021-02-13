6f967cf0440286b76ee614d2b4161063716c5a60
Ventura888 <30947214+Ventura888@users.noreply.github.com>
2021-02-13 14:10:07 +0200

    api test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PteuBossTimer';
+
+  currentBossTime;
+
+  constructor(private http: HttpClient) {
+    this.getBossTime()
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  getBossTime() {
+    this.http.get('https://pristontale.eu/api/api.php?key=c4b90e23c554d10c3c9deadcdbfcf93b').subscribe((data: any) => {
+      this.currentBossTime = data.babel['boss.time.second'];
+    });
+  }
+
 }
